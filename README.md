0bd5ac9dfff462b579b5464b7b65858ebf22fd09
5elenay <71087882+5elenay@users.noreply.github.com>
2021-05-04 10:55:46 +0000

    😊 Güncelleme
    + README Güzelleştirildi.
    + Bugların Çoğu Giderildi.
    + JSON Dosyası Üzerinden Bilgiler Yazılabilir.
    + Kontroller Eklendi.

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
-<h3 align="center">Eba Otomatik Online Ders Girici</h3>
+# Eba Otomatik Derse Girici
+Öğrenciler için ebadaki derslere otomatik girmeye sağlayan bir python programı.
 
+Lütfen kullanmadan önce `selenium` modülünün python sürümünüze indirmeyi unutmayın: [PyPi](https://pypi.org/project/selenium/)
 
-<h3 align="left">Diller ve Kullanılan Araçlar:</h3>
-<p align="left"> <a href="https://www.python.org" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://www.selenium.dev" target="_blank"> <img src="https://raw.githubusercontent.com/detain/svg-logos/780f25886640cef088af994181646db2f6b1a3f8/svg/selenium-logo.svg" alt="selenium" width="40" height="40"/> </a> </p>
+## Bilinen Hatalar
+- ```py
+    selenium.common.exceptions.SessionNotCreatedException: Message: session not created: This version of ChromeDriver only supports Chrome version x
+    Current browser version is x.y.z with binary path C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
+    ```
+    + Bu hata ile karşılaşıyorsanız büyük ihtimal chrome sürümünüz eski. [Burdan](https://chromedriver.chromium.org/downloads) chrome sürümünüze uygun sürümü indirip `Driver` klasörünün içine atınız.
+- Bat/Sh Dosyası Açıldığı Anda Kapanıyor - Hata Veriyor
+    + Bu durumda birkaç olasılık var;
+        + Python3 kullanmıyorsunuz.
+        + Python `PATH` üzerinde yok.
+        + Python yüklü değil.
+        + Selenium yüklü değil.
+    - Windows için;
+        + ```py
+            # Eğer Python3 Kurduğunuzdan Eminseniz;
+            # py -3 main.py
+            # Bu şekilde çalıştırmayı deneyiniz.
+            ```
+
+## Kurulum
++ Python3 kurulu değilse kurunuz.
+    - Python3.8 ve Python3.9 sürümlerinde denendi ve çalıştığı doğrulandı.
+    - Lütfen olabildiğince güncel bir sürüm ile kullanınız.
++ Selenium modülünü python için kurunuz.
+    - `pip install selenium` ya da `py -3 -m pip install -U selenium`
++ Bilgileri doldurunuz.
+    - `ayar.json` Dosyasındaki TC ve Şifre kısmını doldurunuz.
+    - Bu program tamamen açık kaynaklıdır. bilgilerinizin çalınmasına korkmanıza gerek yoktur.
++ Dosyayı çalıştırın.
+    - İsterseniz `start.bat` dosyasını çalıştırın,
+    - İstersenizde `python main.py` ya da `py -3 main.py` şeklinde dosyayı çalıştırın.
+
+# Not:
+Browser üzerinde tek seferlik izin alarmı çıkabilir. `Bir daha sorma` seçeneğini işaretleyerek, sonraki derse girişinde tekrar basmanıza gerek kalmaz.
+
+## Linux/MacOs İçin Ek Not:
+Eba ders uygulaması sadece windows için çalıştığı için linux/macOs için bir driver koymadık. bir yolunu bulduysanız [burdan](https://chromedriver.chromium.org/downloads) kendi işletim sisteminiz için olan bir driveri indirip `Driver` klasörüne atınız.
--- main.py
+++ main.py
@@ -1,50 +1,60 @@
 from selenium import webdriver
 from datetime import datetime as dt
-import datetime
-import time
+import datetime, time, json
 
 browser = webdriver.Chrome(r"Driver\chromedriver")
 browser.maximize_window()
-
 browser.get("https://giris.eba.gov.tr/EBA_GIRIS/student.jsp")
 
-with open('Bilgiler/kimlik.txt', 'r') as file:
-	kimlik = file.read().replace('\n', '')
+def giris_bilgileri_al():
+    with open("./ayar.json", "r", encoding="utf-8") as f:
+        config = json.load(f)
+        f.close()
+    
+    return config['TC'], config['Sifre']
 
-with open('Bilgiler/şifre.txt', 'r') as file:
-	sifre = file.read().replace('\n', '')
-
-while True:
-
-
-    print("Giriş yapılıyor...")
-
-    time.sleep(3)
-
-    tckimlikno = browser.find_element_by_name("tckn")
-    ebasifre = browser.find_element_by_name("password")
-
-    tckimlikno.send_keys(kimlik)
-    ebasifre.send_keys(sifre)
-
+while 1:
+    print('Ebaya Giriş Yapılıyor...')
     time.sleep(2)
 
+    kimlik, sifre = giris_bilgileri_al()
+
+    browser.find_element_by_name("tckn").send_keys(kimlik)
+    browser.find_element_by_name("password").send_keys(sifre)
+
+    time.sleep(2)
     giris = browser.find_element_by_xpath("/html/body/div[2]/div/div[1]/div/div[2]/form/div[5]/button")
     giris.click()
 
     try:
         alert = browser.find_element_by_xpath("/html/body/div[2]/div/div[1]/div/div[2]/div")
-        if alert.text.strip() == 'Kullanıcı adınız veya şifreniz yanlış.' or alert.text.strip() == 'Lütfen kendinize uygun giriş seçeneğinden giriniz.':
+        if alert.text.strip() in ('Kullanıcı adınız veya şifreniz yanlış.', 'Lütfen kendinize uygun giriş seçeneğinden giriniz.'):
             print('Şifre veya T.C kimlik numarası hatalı!')
+            print('5 Saniye İçinde Tekrar Denencek. Lütfen Şifre veya TC Kimlik Numaranızı Düzeltin.')
+
+            kimlik, sifre = giris_bilgileri_al()
+            time.sleep(5)
             continue
 
-    except:
+    except Exception:
+        print('Giriş Yapıldı. Lütfen Bekleyiniz.')
         break
 
-time.sleep(10)
-while True:
-    zaman = browser.find_element_by_xpath('//*[@id="live-times"]').text
-    sonuc = [zaman for zaman in zaman.split(" - ")]
+while 1:
+    print("Lütfen 10 Saniye Bekleyiniz.")
+    time.sleep(10)
+
+    try:
+        zaman = browser.find_element_by_xpath('//*[@id="live-times"]').text
+    except Exception:
+        print('Şuan Canlı Dersiniz Yok Gibi Görünüyor. 5 Saniye Sonra Tekrar Denenicek.')
+        time.sleep(5)
+
+        browser.refresh()
+        continue
+
+    sonuc = zaman.split(" - ")
+    print('Süre Hesaplanıyor...', sonuc)
 
     baslangic = sonuc[0].split(".")
     bitis = sonuc[1].split(".")
@@ -59,10 +69,11 @@ while True:
     ekleme_timedelta = datetime.timedelta(hours=ekleme.hour, minutes=ekleme.minute, seconds=ekleme.second)
 
     if ekleme_timedelta < bitis_toplama and ekleme_timedelta >= baslangic_toplama:
+        print('Ders Zamanı Tespit Edildi. Derse Giriş Yapılıyor...')
         browser.refresh()
 
         ders = browser.find_element_by_xpath("//*[@id='joinMeeting']")
-        ders.click()
+        browser.execute_script("arguments[0].click();", ders)
 
         time.sleep(1)
 
@@ -70,10 +81,21 @@ while True:
         katıl.click()
 
         time.sleep(3)
+        print("POP-UP Bekleniyor...")
         time.sleep(int(browser.find_element_by_xpath("//*[@id='remaining_time_popup_text']").text) + 10)
 
-        if dt.now().minute == bitis[1] and dt.now().hour == bitis[0]:
-            time.sleep(5)
-            browser.refresh()
-            continue
+        ekleme = (datetime.datetime.utcnow() + datetime.timedelta(hours=3))
+        ekleme_timedelta = datetime.timedelta(hours=ekleme.hour, minutes=ekleme.minute, seconds=ekleme.second)
 
+        beklencek = (bitis_toplama - ekleme_timedelta).total_seconds() + 5
+
+        print(f"{beklencek} Saniye Sonra Derse Süresi Biticek.")
+        time.sleep(beklencek)
+        browser.refresh()
+        continue
+    else:
+        print('Ders Zamanı Tespit Edilemedi. 5 Saniye Sonra Yenilenip Tekrar Denenicek...')
+        time.sleep(5)
+
+        browser.refresh()
+        continue
